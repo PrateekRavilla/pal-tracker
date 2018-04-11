c1615c4a35004a9dc077abbdcf690b896474af2c
Jerry Walsh <gfw8266@gmail.com>
2018-04-11 16:47:26 -0600

    pushing local

--- src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
@@ -36,6 +36,7 @@ public class InMemoryTimeEntryRepository implements TimeEntryRepository {
 
         timeEntries.put(id, updatedTimeEntry);
         return updatedTimeEntry;
+
         }
 
     @Override
